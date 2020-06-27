5f7c1a567048c16a6740306939ec28a0c4b2b7d4
oconnormjohn <52138129+oconnormjohn@users.noreply.github.com>
2020-06-27 23:39:02 +0100

    Adding trayscript.js now

---
+++ trayscript.js
@@ -0,0 +1,33 @@
+// Create a tray object 
+class Tray {
+  constructor (traySize, trayContents, trayWeight, trayComment, trayAddress) {
+    this.traySize = traySize;
+    this.trayContents = trayContents;
+    this.trayWeight = trayWeight;
+    this.trayComment = trayComment;
+    this.trayAddress = trayAddress;
+  }
+  
+    isThisSize(traySize) {
+    this.traySize = traySize;
+  }
+    getsFilled(category) {
+    this.trayContents = category;
+  }
+  
+  getsWeighed(trayWeight) {
+    this.trayWeight = trayWeight;
+  }
+  
+  getsComment(trayComment) {
+    this.trayComment = trayComment;
+  }
+
+  getsAddress(trayAddress) {
+    this.trayAddress = trayAddress;
+  }
+}
+
+const tray1 = new Tray('Small', 'Beans', 15.3, "", 'GreenB423');
+
+console.log(tray1);
